974bbf6a9628dbcc7ebe28779df334a1f1ba5df0
Lorenzo Pisani <zeelot3k@gmail.com>
2012-06-17 01:32:35 -0700

    design category list

--- application/classes/view/page/main/index.php
+++ application/classes/view/page/main/index.php
@@ -8,6 +8,34 @@ class View_Page_Main_Index extends Abstract_View_Page {
 
 		return array(
 			array(
+				'name'  => 'Kitchen',
+				'image' => Media::url('images/categories/kitchen.png'),
+				'url'  => URL::site($category_route->uri(array(
+					'category' => 'kitchen'
+				))),
+			),
+			array(
+				'name'  => 'Outdoors',
+				'image' => Media::url('images/categories/outdoors.png'),
+				'url'  => URL::site($category_route->uri(array(
+					'category' => 'outdoors'
+				))),
+			),
+			array(
+				'name'  => 'Kids',
+				'image' => Media::url('images/categories/kids.png'),
+				'url'  => URL::site($category_route->uri(array(
+					'category' => 'kids'
+				))),
+			),
+			 array(
+				'name'  => 'Bedroom',
+				'image' => Media::url('images/categories/bedroom.png'),
+				'url'  => URL::site($category_route->uri(array(
+					'category' => 'bedroom'
+				))),
+			),
+			array(
 				'name'  => 'Bars',
 				'image' => Media::url('images/categories/bar.png'),
 				'url'   => URL::site($category_route->uri(array(
@@ -33,34 +61,6 @@ class View_Page_Main_Index extends Abstract_View_Page {
 				'image' => Media::url('images/categories/fireplaces.png'),
 				'url'  => URL::site($category_route->uri(array(
 					'category' => 'fireplaces'
-				))),
-			),
-			array(
-				'name'  => 'Kids',
-				'image' => Media::url('images/categories/kids.png'),
-				'url'  => URL::site($category_route->uri(array(
-					'category' => 'kids'
-				))),
-			),
-			array(
-				'name'  => 'Kitchen',
-				'image' => Media::url('images/categories/kitchen.png'),
-				'url'  => URL::site($category_route->uri(array(
-					'category' => 'kitchen'
-				))),
-			),
-			array(
-				'name'  => 'Outdoors',
-				'image' => Media::url('images/categories/outdoors.png'),
-				'url'  => URL::site($category_route->uri(array(
-					'category' => 'outdoors'
-				))),
-			),
-			 array(
-				'name'  => 'Bedroom',
-				'image' => Media::url('images/categories/bedroom.png'),
-				'url'  => URL::site($category_route->uri(array(
-					'category' => 'bedroom'
 				))),
 			),
 		);
